aead1782fa66f7d22dbaed1a62183a61c59c1cb7
Nhat Anh <vipfo123@gmail.com>
2022-05-27 09:27:04 +0700

    comit

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace DormitoryManagement
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main(
+        static void Main()
             //Git test
         {
             Application.EnableVisualStyles();
