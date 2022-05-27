a9cb791727e17ab9847fa109c709192a1ac4935f
Nhat Anh <vipfo123@gmail.com>
2022-05-27 09:22:32 +0700

    Git test from Nhựt

--- Program.cs
+++ Program.cs
@@ -12,7 +12,8 @@ namespace DormitoryManagement
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(
+            //Git test
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
